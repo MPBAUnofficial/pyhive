c0ca9c6f5c3be1679fec603325e39dc34180184f
Roberto Bampi <bampi.roberto@gmail.com>
2013-03-18 14:12:28 +0100

    Now really fixed previous issue and bumped version number again

--- hive/extra/django/modifiers.py
+++ hive/extra/django/modifiers.py
@@ -5,9 +5,9 @@ def foreign_key_id(obj, current, *args, **kwargs):
     for field in obj._meta.fields:
         if isinstance(field, (ForeignKey, OneToOneField)):
             if field.name in current:
-                field = current[field.name]
-                if field is not None and hasattr(field, 'pk'):
-                    current[field.name] = field.pk
+                field_instance = current[field.name]
+                if field_instance is not None and hasattr(field_instance, 'pk'):
+                    current[field.name] = field_instance.pk
                 else:
                     current[field.name] = None
     return current
